98bc25944ecc26079cb563b8a602449c15b5a202
ilahaS <80361058+IlahaS@users.noreply.github.com>
2024-04-18 20:25:19 +0400

    Initial Commit

---
+++ BRTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BRTask
+//
+//  Created by Ilahe Samedova on 18.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
